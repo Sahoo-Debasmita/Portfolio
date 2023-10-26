f575210648b0b6d69a99fd6586ce3bf2ae47b7ea
Debasmita Sahoo <sdebasmita974@gmail.com>
2023-10-26 21:01:20 +0530

    Portfolio Section

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import SocialLinks from './Components/SocialLinks'
+import About from './Components/About'
+import Portfolio from './Components/Portfolio'
 
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
     <>
      <Navbar/>
      <Home/>
-     
+     <About/>
+     <Portfolio/>
      <SocialLinks/>
     </>
   )
---
+++ MyPortfolio/src/Components/Portfolio.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Bubble from '../assets/Bubble.jpg'
+import MusicPlayer from '../assets/music player.jpg'
+import PasswordGenerator from '../assets/password generator.png'
+import TailwindCard from '../assets/TailwindCard.png'
+
+const Portfolio = () => {
+ const portfolio=[
+  {
+    id:1,
+    src:Bubble,
+    Name:"Bubble Game using JS"
+  },
+  {
+    id:2,
+    src:MusicPlayer,
+    Name:"Mini Music Player"
+  },
+  {
+    id:3,
+    src:PasswordGenerator,
+    Name:"Random Password Generator"
+  },
+  {
+    id:4,
+    src:TailwindCard,
+    Name:"FB login page with Tailwind"
+  },
+ ]
+
+  return (
+    <div name="Portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white w-full md:h-screen'>
+          <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+              <p className='text-4xl font-bold border-b-4 inline border-gray-500'>Portfolio</p>
+              <p className='py-6 text-lg'>Check out some of my work here</p>
+            </div>
+            <div className='grid md:grid-cols-3 sm:grid-cols-2 gap-8 px-12 sm:px-0'>
+            {portfolio.map(({id,src,Name})=>(
+                <div key={id} className='shadow-md shadow-gray-600 x '>
+                <img src={src} alt="" className=' w-full h-40 object-cover max-h-screen duration-200 hover:scale-105 rounded-md' />
+                <div className='flex justify-center mt-2 font-semibold'>
+                {Name}
+                </div>
+                <div className='flex items-center justify-center'>
+                  <button className='bg-blue-700 w-1/2 px-1 py-2.5 m-2 duration-200 hover:scale-105'>Demo</button>
+                  <button className='bg-blue-700 w-1/2 px-1 py-2.5 m-2 duration-200 hover:scale-105'>Code</button>
+                </div>
+              </div>
+              ))}
+            </div>
+          </div>
+
+   </div>
+  )
+}
+
+export default Portfolio
