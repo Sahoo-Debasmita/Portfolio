8020b37fb674adea6be5d11ecadfd508b534f52d
Debasmita Sahoo <sdebasmita974@gmail.com>
2023-11-09 19:08:33 +0530

    Fixed a bug related to form in contact page

--- MyPortfolio/src/Components/Contact.jsx
+++ MyPortfolio/src/Components/Contact.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
         </div>
         <div className='flex justify-center items-center'>
             <form action="https://getform.io/f/b09d07c2-20ec-43ef-b4ce-512d6b92e11b" method='POST' className='flex flex-col w-full md:w-1/2'>
-                <input type="text" name='name' placeholder='Enter your name here' className='p-2 focus:outline-none bg-transparent border-2 rounded-md text-white' />
-                <input type="email" name='email' placeholder='Enter your E-Mail here' className='p-2 my-4 focus:outline-none bg-transparent border-2 rounded-md text-white' />
-                <textarea name="message" rows="10" placeholder='Enter your message here'
+                <input type="text" required name='name' placeholder='Enter your name here' className='p-2 focus:outline-none bg-transparent border-2 rounded-md text-white' />
+                <input type="email" required name='email' placeholder='Enter your E-Mail here' className='p-2 my-4 focus:outline-none bg-transparent border-2 rounded-md text-white' />
+                <textarea name="message" required rows="10" placeholder='Enter your message here'
                 className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
                 <button className='bg-gradient-to-b from-cyan-500 to-blue-500 text-white px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
             </form>
